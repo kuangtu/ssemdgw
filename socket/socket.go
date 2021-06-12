ae8327432683009c842826feda896b5cf96b6fa7
kuangtu <kuangtuweb@gmail.com>
2021-06-12 23:29:50 +0800

    mdgw

--- socket/socket.go
+++ socket/socket.go
@@ -5,11 +5,18 @@ import (
     "net"
 )
 
-
+//socket管理结构体
 type MdgwSock struct {
     laddr, raddr *net.TCPAddr
     lconn, rconn io.ReadWriteCloser
 }
+
+
+//发送socket连接
+
+//socket监听
+
+//socket读取
 
 
 
